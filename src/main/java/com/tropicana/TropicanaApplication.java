ef83144ea8ab85124a46ec115787331012b5862e
박지환 <mycook3@naver.com>
2024-12-14 14:45:14 +0900

    first commit

---
+++ src/main/java/com/tropicana/TropicanaApplication.java
@@ -0,0 +1,13 @@
+package com.tropicana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TropicanaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TropicanaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tropicana/TropicanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tropicana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TropicanaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
