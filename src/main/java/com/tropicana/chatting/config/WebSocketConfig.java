7730a46acf2a6da62730720e284de717b065854a
frog-slayer <pj0642@gmail.com>
2024-12-21 22:53:51 +0900

    [fix] STOMP를 사용하지 않고 순수하게 WebScoekt만을 사용해 구현하기 위해 커밋을 revert

--- src/main/java/com/tropicana/chatting/config/WebSocketConfig.java
+++ src/main/java/com/tropicana/chatting/config/WebSocketConfig.java
@@ -2,26 +2,21 @@ package com.tropicana.chatting.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @Configuration
-@EnableWebSocketMessageBroker
+@EnableWebSocket
 @RequiredArgsConstructor
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+public class WebSocketConfig implements WebSocketConfigurer {
+
+  private final WebSocketHandler webSocketHandler;
 
   @Override
-  public void configureMessageBroker(MessageBrokerRegistry config) {
-    config.enableSimpleBroker("/topic");
-    config.setApplicationDestinationPrefixes("/app");
-  }
-
-  @Override
-  public void registerStompEndpoints(StompEndpointRegistry registry) {
-    registry.addEndpoint("/chatting")
-        .setAllowedOrigins("*")
-        .withSockJS();
+  public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+    registry.addHandler(webSocketHandler, "ws/chat")
+        .setAllowedOrigins("*");
   }
 }
